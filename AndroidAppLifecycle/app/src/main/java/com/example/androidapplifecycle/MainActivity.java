3547294e4eb9220e11329391e4f99a4fac817871
sweetalesious <60169171+sweetalesious@users.noreply.github.com>
2020-04-05 19:48:11 +0530

    Add files via upload

---
+++ AndroidAppLifecycle/app/src/main/java/com/example/androidapplifecycle/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.androidapplifecycle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(getApplicationContext().toString(), "OnStart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(getApplicationContext().toString(), "OnStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(getApplicationContext().toString(), "OnDestroy");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(getApplicationContext().toString(), "OnPause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(getApplicationContext().toString(), "OnResume");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(getApplicationContext().toString(), "OnRestart");
+    }
+}
